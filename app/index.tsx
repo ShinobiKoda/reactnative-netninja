42fef1b795688ab004c74975433fca2e65834b3b
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-12 13:01:16 +0100

    Refactor code structure for improved readability and maintainability

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("../assets/images/favicon.png")}
+        style={styles.image}
+      />
+
+      <Text style={styles.title}>List</Text>
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
+
+      <View style={styles.card}>
+        <Text>Hello this is a card.</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+
+  card: {
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    shadowColor: "#000",
+    shadowOffset: { width: 4, height: 4 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+
+  image: {
+    marginVertical: 20,
+  },
+});
