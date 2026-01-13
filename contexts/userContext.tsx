bb9f76dc89a0cc57fe185ae8dffb7b6ee8967ff0
ShinobiKoda <adebiyipacsc2022@futa.edu.ng>
2026-01-13 15:29:18 +0100

    feat: enhance user context with authentication check and initial user value retrieval

--- contexts/userContext.tsx
+++ contexts/userContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, type ReactNode } from "react";
+import { createContext, useState, type ReactNode, useEffect } from "react";
 import { account } from "../lib/appwrite";
 import { ID } from "react-native-appwrite";
 
@@ -13,6 +13,7 @@ export type UserContextValue = {
   login: (email: string, password: string) => Promise<void>;
   register: (email: string, password: string) => Promise<void>;
   logout: () => Promise<void>;
+  authChecked: boolean
 };
 
 export const userContext = createContext<UserContextValue | null>(null);
@@ -21,6 +22,7 @@ type UserProviderProps = { children: ReactNode };
 
 export const UserProvider = ({ children }: UserProviderProps) => {
   const [user, setUser] = useState<User>(null);
+  const [authChecked, setAuthChecked]= useState<boolean>(false);
 
   const login = async (email: string, password: string) => {
     try {
@@ -47,8 +49,23 @@ export const UserProvider = ({ children }: UserProviderProps) => {
     setUser(null);
   };
 
+  const getInitialUserValue = async() =>{
+    try{
+      const response = await account.get()
+      setUser({ id: response.$id, email: response.email });
+    }catch(error:any){
+      setUser(null)
+    }finally{
+      setAuthChecked(true);
+    }
+  }
+
+  useEffect(()=>{
+    getInitialUserValue();
+  }, [])
+
   return (
-    <userContext.Provider value={{ user, setUser, login, register, logout }}>
+    <userContext.Provider value={{ user, setUser, login, register, logout, authChecked }}>
       {children}
     </userContext.Provider>
   );
